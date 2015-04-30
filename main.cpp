ac8cab89e9cf1f41a49ba80c474dad6610638bef
AndrewWesterman <awesterman1507@gmail.com>
2015-04-30 13:31:01 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,2 +1,2 @@
-// Main file that parses throught the log file and implements the modules
+// Main file that parses through the log file and implements the modules
 // we have created to produce an output file.

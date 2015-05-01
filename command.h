1a27e46595435db91f704a8518f6e199d048db3a
Daniel Ceja <cejad_08@fullerton.edu>
2015-04-30 21:10:42 -0700

    Finished header file for command

---
+++ command.h
@@ -0,0 +1,18 @@
+#ifndef __COMMAND_H
+#define __COMMAND_H
+
+using namespace std;
+
+class Command {
+  int line_num;
+  string name;
+  string type;
+  int num_words;
+public:
+  Command();
+  void set_values(int line_num, string name, string type, string num) ;
+  int get_num_of_words() const;
+  void print() const;
+};
+
+#endif

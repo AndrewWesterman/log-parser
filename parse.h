d42fb9287f139659e2f1eadd4fb2094861e7a53b
AndrewWesterman <awesterman1507@gmail.com>
2015-05-02 16:33:29 -0700

    Unified formatting

--- parse.h
+++ parse.h
@@ -4,19 +4,19 @@
 using namespace std;
 
 class Data_Info {
-  string address;
-  string data;
-  string size;
-  string kind;
-  string time;
-public:
-  Data_Info();
-  void parse_line(string a);
-  string get_address();
-  string get_data();
-  string get_size();
-  string get_kind();
-  string get_time();
+    string address;
+    string data;
+    string size;
+    string kind;
+    string time;
+ public:
+    Data_Info();
+    void parse_line(string a);
+    string get_address();
+    string get_data();
+    string get_size();
+    string get_kind();
+    string get_time();
 };
 
 #endif

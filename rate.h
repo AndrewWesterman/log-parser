459578a25678516597edb5315b71cc29fda85e1a
Duy Do <ddo@csu.fullerton.edu>
2015-05-02 16:29:05 -0700

    Module to calculate the rate for one kind of command

---
+++ rate.h
@@ -0,0 +1,24 @@
+#ifndef __RATE_H
+#define __RATE_H
+
+
+using namespace std;
+
+
+const unsigned int  KILOBITS = 1000;
+const unsigned int MEGABITS = 1000000;
+const unsigned long GIGABITS = 1000000000;
+const unsigned long TERABITS = 1000000000000;
+
+class Rate {
+  string name;
+  double total_time;
+  long count;
+public:
+  Rate();
+  void set_name(string name);
+  void set_time(string current_time);
+  void print(ofstream& outFile);
+};
+
+#endif

05e953184ef4734dcd649748a852bb4617c29177
Crystal Juarez <freespirit13@csu.fullerton.edu>
2015-05-01 19:38:27 -0700

    Conversion of values from bin, hex, dec

--- convert.cpp
+++ convert.cpp
@@ -1,1 +1,48 @@
 //Class responsible for converting values between hex, binary, and decimal
+#include <iostream>
+#include <string>
+#include <bitset>
+#include <sstream>
+#include "convert.h"
+
+using namespace std;
+
+
+//Pass in the string
+string hex_to_bin(string s){
+
+  stringstream hex_to_bin;
+  int n;
+  string address_in_binary = "";
+
+  if ( s.length() < 5 ) {
+    hex_to_bin << hex << s;
+    hex_to_bin >> n;
+    bitset<16> b(n);
+    address_in_binary = b.to_string();
+  }
+  else {
+    hex_to_bin << hex << s;
+    hex_to_bin >> n;
+    bitset<32> b(n);
+    address_in_binary = b.to_string();
+  }
+  
+  return address_in_binary;
+}
+
+int bin_to_dec(string address_in_binary) {
+ int dec;
+ stringstream bin_to_dec;
+ bitset<32> set(address_in_binary);
+ bin_to_dec<< hex << set.to_ulong() <<endl;
+ bin_to_dec>>dec;
+ return dec;
+}
+
+int hex_to_dec(string s) {
+  string address_in_bin = hex_to_bin(s);
+  int address_in_dec = bin_to_dec(address_in_bin);
+  return address_in_dec;
+}
+
